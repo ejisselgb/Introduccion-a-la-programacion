fe16479b220734fc28c04680fded055a5a17daa3
Erika Jissel Gutierrez Beltran <erika.beltran@vtex.com.br>
2022-01-23 13:44:22 -0500

    Ajuste clase 2022-1

--- 7.Clase 7 - parcial/Solución parcial/Pregunta11.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Ingrese el número que desea validar "))
-m = int(input("Ingrese el numero del divisor "))
-
-calculo_mod = n % m
-
-if calculo_mod == 0:
-    print("El número es múltiplo de: " + str(m))
-else:
-    print("El número NO es múltiplo de: " + str(m))
--- Solucionario parcial final/validaciones_iteradores.py
+++
@@ -1,8 +0,0 @@
-numero = 1
-while numero <= 50:
-    print("El numero final es: "+str(numero))
-    numero = numero + 1
-
-
-for i in range (1,11):
-    print(i)
