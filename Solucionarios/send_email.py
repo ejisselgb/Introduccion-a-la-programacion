88b8231f3fc35facd8df0eb277cdd1b0b4bbceb4
Erika Jissel Gutierrez Beltran <erika.beltran@vtex.com.br>
2022-06-07 10:00:39 -0500

    Se agregan comentarios

--- Solucionarios/send_email.py
+++ Solucionarios/send_email.py
@@ -4,11 +4,17 @@ import email.utils
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+# Se crea clase para las configuraciones estaticas que permitirán el envio de correos
+
 
 class Settings():
+    # Método con valores estáticos de configuración
+    # Se retorna la información con las credenciales.
     @staticmethod
     def config_data():
+        # REEMPLAZAR POR VALORES ENTREGADOS VÍA TEAMS - NO SUBIR ARCHIVO CON CREDENCIALES AL REPOSITORIO
         USERNAME_SMTP = "XXXXX"
+        # REEMPLAZAR POR VALORES ENTREGADOS VÍA TEAMS - NO SUBIR ARCHIVO CON CREDENCIALES AL REPOSITORIO
         PASSWORD_SMTP = "XXXXX"
         HOST = "email-smtp.us-east-2.amazonaws.com"
         AWS_REGION = "us-east-2"
@@ -22,8 +28,11 @@ class Settings():
             "port": PORT
         }
 
+# Se crea clase para el envio de correo que recive un emisor, nombre de quien envia, remitente, titulo del mesanje y mensaje en texto plano
+
 
 class SendEmail():
+    # Constructor para recibir parámetros al momento de inicializar la clase
     def __init__(self, sender, sendername, recipient, subject, message):
         self.sender = sender
         self.sendername = sendername
@@ -31,6 +40,8 @@ class SendEmail():
         self.subject = subject
         self.message = message
 
+    # Método encargado del envío del mensaje
+    # NO REEMPLAZAR NADA AQUÍ
     def send(self):
         msg = MIMEMultipart()
         msg['Subject'] = self.subject
@@ -55,6 +66,7 @@ class SendEmail():
             print("Error ", e)
 
 
-send_email = SendEmail("erika.giselle.gb@gmail.com", "Erika Gutierrez", "erika.giselle.gb@gmail.com",
+# Se invoca la clase y se le otorgan los parámetros necesarios para el envío de correo
+send_email = SendEmail("erika.giselle.gb@gmail.com", "Erika Gutierrez", "erika.giselle.gb@hotmail.com",
                        "Prueba correo estudiantes", "Esto es una prueba de correo para el trabajo final de los estudiantes")
 send_email.send()
